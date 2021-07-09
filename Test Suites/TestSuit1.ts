e834eb2c0ef1a97465041df63ef367c893949a9e
edgarrodriguez0330 <edgarrodriguezg03@gmail.com>
2021-07-09 10:05:47 -0400

    Commit

---
+++ Test Suites/TestSuit1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuit1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>4845e6c6-b5fd-4b6a-ab2f-34a6ba5f028b</testSuiteGuid>
+   <testCaseLink>
+      <guid>81a5c499-4a37-4bfb-9a2f-8ab8291187f0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
